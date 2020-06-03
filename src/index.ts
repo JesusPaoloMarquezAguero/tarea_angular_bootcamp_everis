e92db4aeb74d5966573c3e34eb0764ef28ef2c8a
Jesus Paolo Marquez Agüero <jmarquag@everis.com>
2020-06-03 18:47:07 -0500

    Se añadio otros estudiantes

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import {Bootcamp} from './classes/bootcamp.class';
 
 let curso_bootcamp=new Bootcamp('Angular','Everis');
 
-curso_bootcamp.agregarEstudiante('julio')
-curso_bootcamp.agregarEstudiante('raul')
+curso_bootcamp.agregarEstudiante('cristian')
+curso_bootcamp.agregarEstudiante('paul')
+curso_bootcamp.agregarEstudiante('paolo')
 
 curso_bootcamp.presentarClase();
 document.body.innerHTML = "<h1>Esta es la clase de" + curso_bootcamp.nombre + ", de la empresa: " + curso_bootcamp.empresa 
